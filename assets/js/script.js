a1c71cdf931431eb8ff13262daa13b0c4ea9ee2b
Anderice <42161586+Anderice@users.noreply.github.com>
2021-01-01 16:59:57 +0800

    v64

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,15 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("pageProgress").style.width = scrolled + "%";
 }
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("breadcrumbs").style.top = "0";
+  } else {
+    document.getElementById("breadcrumbs").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
