ff564b89c07847e1b1550e5887e6681cc95dd109
Anderice <42161586+Anderice@users.noreply.github.com>
2021-01-02 02:01:08 +0800

    v55

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,28 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
 
-function myFunction() {
+/* When the user scrolls down*/
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  breadcrumbsProgressbar()
+  breadcrumbsHide()
+}
+
+// load progressbar
+function breadcrumbsProgressbar() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("pageProgress").style.width = scrolled + "%";
 }
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+// Hide breadcrumbs
+function breadcrumbsHide() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("breadcrumbs").style.top = "0";
+    document.getElementById("pageProgress").style.background = "var(--gray-4)";
   } else {
-    document.getElementById("breadcrumbs").style.top = "-70px";
+    document.getElementById("breadcrumbs").style.top = "-64px";
+    document.getElementById("pageProgress").style.background = "var(--primary)";
   }
   prevScrollpos = currentScrollPos;
 }
